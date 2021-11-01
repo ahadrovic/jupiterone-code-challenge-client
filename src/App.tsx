a84e0d527ab82cc0ef3bf63ae2b2fea114cda509
Adem Hadrovic <ademhadrovic@gmail.com>
2021-11-01 00:47:43 -0400

    modified app ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { Container } from '@mui/material';
+import Header from './screens/Header';
 import Dashboard from './screens/Dashboard';
+
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <Container maxWidth="lg">
+    <Container className={styles.appContainer}>
+      <Header />
       <Dashboard />
     </Container>
   );
