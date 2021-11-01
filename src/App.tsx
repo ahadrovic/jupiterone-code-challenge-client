64702e7d9874b769a5acbcef569a00382bd444d8
Adem Hadrovic <ademhadrovic@gmail.com>
2021-10-31 23:06:38 -0400

    installed material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo_white.png';
-import './App.css';
 
-function App() {
+import { Container } from '@mui/material';
+import Dashboard from './screens/Dashboard';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Dashboard />
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+import styles from './Dashboard.module.css';
+
+const Dashboard = () => {
+  return (
+    <Box className={styles.dashboardBox}>
+      <span>Content will go here</span>
+    </Box>
+  );
+};
+
+export default Dashboard;
