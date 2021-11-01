cf80f346dd0eccb41954670c126dd9824b1e7123
Adem Hadrovic <ademhadrovic@gmail.com>
2021-11-01 03:45:39 -0400

    added field required validation

--- src/app/dashboard/Form.tsx
+++ src/app/dashboard/Form.tsx
@@ -4,33 +4,56 @@ import {
   Input,
   InputLabel,
   FormControl,
-  FormHelperText,
   Button,
   Typography,
+  FormHelperText,
 } from '@mui/material';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import { FormInputs } from '../../types/form';
 
 import styles from './Form.module.css';
 
 type FormProps = {
-  onSubmit: () => void;
+  onSubmit: (data: FormInputs) => void;
 };
 
 const Form = ({ onSubmit }: FormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm: SubmitHandler<FormInputs> = (data) => onSubmit(data);
+
   return (
-    <Box component="form" className={styles.formBox}>
+    <Box
+      component="form"
+      className={styles.formBox}
+      onSubmit={handleSubmit(submitForm)}
+    >
       <Typography variant="h5" style={{ color: '#333333' }}>
         Enter an email address to get started
       </Typography>
       <br />
       <div className={styles.formBoxDiv}>
         <FormControl className={styles.formControl}>
-          <InputLabel htmlFor="my-input">Email address</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
-          <FormHelperText id="my-helper-text">
-            We'll never share your email.
-          </FormHelperText>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input
+            error={!!errors.email}
+            id="email"
+            aria-describedby="helper-text"
+            {...register('email', { required: true })}
+          />
+          {errors.email && (
+            <FormHelperText id="helper-text" error={true}>
+              This field is required
+            </FormHelperText>
+          )}
         </FormControl>
-        <Button onClick={onSubmit} variant="outlined">
+        <Button type="submit" variant="outlined">
           Submit
         </Button>
       </div>
