6d74627f8d56e50ca25ac12c089a962264fdc916
Adem Hadrovic <ademhadrovic@gmail.com>
2021-11-01 02:45:48 -0400

    created table header

---
+++ src/app/dashboard/breachesTable/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Box, TableHead, TableRow } from '@mui/material';
+import { IHeaderCell, Order } from '../../../types/table';
+
+import HeaderCell from './header/Cell';
+
+interface IProps<T> {
+  order: Order;
+  orderBy: keyof T;
+  headerCells: IHeaderCell<T>[];
+}
+interface IHandlers<T> {
+  onSort: (event: React.MouseEvent<unknown>, property: keyof T) => void;
+}
+
+export type TableHeadProps<T> = IProps<T> & IHandlers<T>;
+
+const Header = <T,>({
+  order,
+  orderBy,
+  headerCells,
+  onSort,
+}: TableHeadProps<T>) => {
+  const onSortClick =
+    (property: keyof T) => (event: React.MouseEvent<unknown>) => {
+      onSort(event, property);
+    };
+  return (
+    <TableHead>
+      <TableRow>
+        {headerCells.map((cell) => (
+          <HeaderCell
+            {...cell}
+            order={order}
+            orderBy={orderBy}
+            onSortClick={onSortClick}
+          />
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default Header;
